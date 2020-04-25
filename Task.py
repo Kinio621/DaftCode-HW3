d43a3bc76fefb2f2e9b5b26befd52383dfc68024
Kinio621 <44881531+Kinio621@users.noreply.github.com>
2020-04-25 14:15:24 +0200

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from hashlib import sha256
+from fastapi import FastAPI, Response, Cookie, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+@app.post("/login")
+def login(credentials: HTTPBasicCredentials = HTTPBasic()):
+    if(secrets.compare_digest(credentials.username, "trudnY") and secrets.compare_digest(credentials.password, "PaC13Nt")):
+        response=RedirectResponse(url='/welcome')
+        return response
+    else:
+        raise HTTPException(status=401, detail='failed to log in')
+
 @app.get("/welcome")
-def root():
+def welcome():
     return {"message": "Hello there"}
 
 @app.get("/")
-def root():
+def default():
     return {"message": "Hello there"}
