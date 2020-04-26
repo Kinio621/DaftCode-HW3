e8f3fa183c8b1b5dd836b2c81f55c7b8bcbe7e35
Kinio621 <44881531+Kinio621@users.noreply.github.com>
2020-04-26 22:01:17 +0200

    Update Task.py

--- Task.py
+++ Task.py
@@ -17,19 +17,31 @@ def verify(credentials: HTTPBasicCredentials = Depends(security)):
         session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
         app.sessions[session_token]=credentials.username
         return session_token
-
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect login or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-   
+def verify_cookie(session_token: str = Cookie(None)):
+    if session_token not in app.sessions:
+        session_token = None
+    return session_token
+
 @app.post("/login")
 def login(response: Response, session_token: str = Depends(verify)):
     response.status_code = status.HTTP_302_FOUND
     response.headers["Location"] = "/welcome"
     response.set_cookie(key="session_token", value=session_token)
+
+@app.post("/logout")
+def logout(response: Response, session_token: str = Depends(verify_cookie)):
+     if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return "No access"
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = "/"
+    app.sessions.pop(session_token)
 
 @app.get("/welcome")
 def welcome():
